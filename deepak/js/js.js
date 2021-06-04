221fcbc9e2477589f79c0bd8bcc627d5bc169398
Deepak <65610599+DeepakKumarNaik@users.noreply.github.com>
2021-06-04 16:31:19 +0530

    Add files via upload

--- deepak/js/js.js
+++ deepak/js/js.js
@@ -1,6 +1,4 @@
 
-
-
 var theme_color=document.getElementById('theme-color');
     var x_t_c=0;
 	var y_t_c=0;
@@ -43,12 +41,27 @@ const p = Array(Math.floor(w / 10) + 1).fill(0);
 
 const random = (items) => items[Math.floor(Math.random() * items.length)];
 
-const hex = "0123456789DEEPAK".split("");
-
+const hex = "000DEEPAK00KUMAR00NAIK000".split("");
+var bg_r=0;
+var bg_g=255;
+var bg_b=255;
 setInterval(() => {
   ctx.fillStyle = "rgba(0,0,0,.05)";
+  
   ctx.fillRect(0, 0, w, h);
-  ctx.fillStyle = "rgba(0,255,255,1)";
+  ctx.fillStyle = "rgba("+bg_r+","+bg_g+","+bg_b+",1)";
+  bg_r=bg_r+0;
+    bg_g=bg_g+255;
+	  bg_b=bg_b+255;
+	  if(bg_r>255){
+		  bg_r=0;
+	  }
+	  if(bg_g>255){
+		 bg_g=255; 
+	  }
+	 if(bg_b>255){
+		 bg_b=0;
+	 }
   p.map((v, i) => {
     ctx.fillText(random(hex), i * 10, v);
     p[i] = v >= h || v > 50 + 10000 * Math.random() ? 0 : v + 10;
